3d61a3d65d75da9d069a73bd19dbb4d068813d45
Varakh <varakh@varakh.de>
2021-04-21 21:28:29 +0200

    Update dependencies

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,4 +1,4 @@
 export const appConstants = {
-	TITLE: window && window._env_ && window._env_.REACT_APP_APP_TITLE || process.env.REACT_APP_APP_TITLE,
+	TITLE: (window && window._env_ && window._env_.REACT_APP_APP_TITLE) || process.env.REACT_APP_APP_TITLE,
 	PRODUCTION: process.env.NODE_ENV && process.env.NODE_ENV === 'production',
 };
